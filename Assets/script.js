0111cb00525e26144c115a37ac69c9784462498f
Jose Espinoza <jose1234.je90@gmail.com>
2022-06-29 16:01:52 -0500

    reorganized arrays and confirm prompts created no criteria response

--- Assets/script.js
+++ Assets/script.js
@@ -7,53 +7,37 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
+
+// password criteria options
+var specialCharacters = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+var numericCharacters = "0123456789";
+var letters = "abcdefghijklmnopqrstuvwxyz";
+
+//  generate password by combining arrays and choosing random
+function generatePassword() {
+  var length = askForLength(8, 128);
+  // if length does not meet requirements
+  if (!length) {
+    return null;
+  }
+
+  var lowercase = confirm("Use lowercase letters?");
+  var uppercase = confirm("Use uppercase letters?");
+  var numbers = confirm("Use numeric characters?");
+  var special = confirm("Use special characters?");
+
+  // if no criteria is selected it should show an error
+  if (!(lowercase || uppercase || numbers || special)) {
+    window.alert("No criteria selected. please select desired password criteria!");
+    return null
+  }
+
+  var generatedPassword = passwordGeneration(length, lowercase, uppercase, numbers, special);
+  return generatedPassword;
+}
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-function generatePassword() {
-
-//PseudoCode
-
-  //let pwdLenght = prompt("How many characters?")
-var  pwdLength = prompt("please enter password length");
-//   console.log(pwdLength)
-
-// confirm - lowercase, uppercase, numeric, special characters
-var lowCase = confirm("would you like to include lowercase letters?");
-// console.log(lowCase);
-
-var upCase = confirm("Would you like to use uppercase numbers?");
-
-// let numbers = confirm("Do you want numbers"
-var numbers = confirm("would you like to use numbers");
-//console.log(numbers);
-
-var speChar = confirm("Would you like to use special characters?");
-// create var to hold answer to prompt/confirm
-
-// fuction to validate user answers
-var upperCase = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var num = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
-var specCharacters = ['~', '`', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '{', '}', '[', ']', '|', '/', ':', ';', "'", '"', '<', '>', ',', '.', '?'] ;
-// if and equality operators
-  //if(numbers) {
-
-  // }
-
-  // List of numbers, special chars, uppercase and lowercase
-    // array 
-    // combine all arrays in 1 array
-    // random choose characters
-    //create a string set equal to password var
-    // array method to check if something is in array
-// generate random password
-// return password
-
-//google
-
-  return password;
-}
