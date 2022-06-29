87bfa19c3cf4b4a7188b1da122aebe85aad6e207
Jose Espinoza <jose1234.je90@gmail.com>
2022-06-28 19:40:40 -0500

    added prompts and confirms, also added arrays for password options

--- Assets/script.js
+++ Assets/script.js
@@ -14,19 +14,31 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
-//your code here
 
 //PseudoCode
 
-// prompt = length
   //let pwdLenght = prompt("How many characters?")
-  // console.log(pwdLength)
+var  pwdLength = prompt("please enter password length");
+//   console.log(pwdLength)
+
 // confirm - lowercase, uppercase, numeric, special characters
-  // let numbers = confirm("Do you want numbers")
-  //console.log(numbers);
+var lowCase = confirm("would you like to include lowercase letters?");
+// console.log(lowCase);
+
+var upCase = confirm("Would you like to use uppercase numbers?");
+
+// let numbers = confirm("Do you want numbers"
+var numbers = confirm("would you like to use numbers");
+//console.log(numbers);
+
+var speChar = confirm("Would you like to use special characters?");
 // create var to hold answer to prompt/confirm
 
 // fuction to validate user answers
+var upperCase = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var num = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var specCharacters = ['~', '`', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '{', '}', '[', ']', '|', '/', ':', ';', "'", '"', '<', '>', ',', '.', '?'] ;
 // if and equality operators
   //if(numbers) {
 
